363bd05a576079687d3a42c5adf9550b6b099ff1
Bagus Tris Atmaja <b-atmaja@aist.go.jp>
2024-06-04 10:42:52 +0900

    Add files via upload

---
+++ fp_if.m
@@ -0,0 +1,19 @@
+close all 
+clear all 
+clc 
+
+% read the wav file
+%r=11025; 
+%[d]=yy12; 
+[d,r]=wavread('clear.wav'); 
+[I,S]=ifgram(d,256,256,128,r); 
+[R,M]=extractrax(abs(S)); 
+a=size(R); 
+F=colinterpvals(R,I); 
+P=-colinterpvals(R,unwrap(angle(S))); 
+size(F);
+tt=[1:a(2)]*128/r;          % deault spectogram step is NFFT/2 i.e 128
+
+
+size(tt);
+specgram(d,256,r); hold on; figure; plot (tt,F','r');
